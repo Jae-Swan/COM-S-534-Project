6de1e77e1d78aa6272984ae2847f1e87681afe2e
AdjustedMold97 <jaeswan@outlook.com>
2023-09-27 10:30:48 -0500

    Update t-j.py
    fixed logic

--- src/simuq/systems/t-j.py
+++ src/simuq/systems/t-j.py
@@ -18,25 +18,23 @@ qs = QSystem()
 fermions = [Fermion(qs) for _ in range(N)]
 
 sops = []  # spin operators
-for i in range(L):
-    up, down = i * 2, i * 2 + 1
-    sops[i] = hbar / 2 * (fermions[up].c * pops * fermions[down].a + fermions[down].c * pops * fermions[up].a)
-# TODO verify accuracy of this
+for k in range(L):
+    sops[k] = fermions[k].c * pops * fermions[k].a
 
 t = 0.1          # hopping amplitude
 U = 0.3          # strength of on-site interaction
-J = (t * t) / U  # exchange interaction TODO verify accuracy
+J = (t * t) / U  # exchange interaction
 hh = 0           # hopping term
 ho = 0           # on-site interaction term
 
-for i in range(L - 1):  # first term same as Hubbard model
+for i in range(L - 1):
     for s in range(2):
         f1, f2 = 2 * i + s, 2 * (i + 1) + s
-        hh += -t * (fermions[f1].c * fermions[f2].a + fermions[f2].c * fermions[f1].a)
+        hh += (-t * (fermions[f1].c * fermions[f2].a + fermions[f2].c * fermions[f1].a)) + (J * (sops[f1] * sops[f2]))
+#                                ^ t term ^                                                     ^ J term ^
 
-for i in range(L):
-    for j in range(L):
-        ho += J * (sops[i] * sops[j])
+for i in range(L - 1):
+    ho += J * (sops[i] * sops[i + 1])
 
 h = hh + ho
 qs.add_evolution(h, t)
