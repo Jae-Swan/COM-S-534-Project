96a5d76f246d7599ba09599cb2811d2681942310
AdjustedMold97 <jaeswan@outlook.com>
2023-10-06 16:33:47 -0500

    Update t-j.py
    fixed spin operator calculation

--- src/simuq/systems/t-j.py
+++ src/simuq/systems/t-j.py
@@ -2,14 +2,6 @@ __author__ = "Jae Swanepoel"
 
 from simuq.environment import Fermion
 from simuq.qsystem import QSystem
-import numpy as np
-
-# Pauli operators
-# Not sure if these operators are defined elsewhere
-pX = [[0, 1], [1, 0]]
-pY = [[0, -1j], [1j, 0]]
-pZ = [[1, 0], [0, -1]]
-pops = [pX, pY, pZ]
 
 L = 5  # number of lattice sites
 N = 2 * L  # number of fermionic systems
@@ -17,8 +9,12 @@ N = 2 * L  # number of fermionic systems
 qs = QSystem()
 fermions = [Fermion(qs) for _ in range(N)]
 
-sops = [0 for _ in range(L)]  # spin operators
-#TODO define sops
+sops = []  # spin operators
+for i in range(L):
+    u, d = fermions[i], fermions[i + 1]
+    sops[i] = [0.5 * (u.c * d.a + d.c * u.a),
+               0.5j * (d.c * u.a - u.c * d.a),
+               0.5 * (u.c * u.a - d.c * d.a)]
 
 t = 0.1  # hopping amplitude
 U = 0.3  # strength of on-site interaction
@@ -28,10 +24,9 @@ hJ = 0  # J term
 
 for i in range(L - 1):
     iu, id, ju, jd = fermions[2 * i], fermions[2 * i + 1], fermions[2 * (i + 1)], fermions[2 * (i + 1) + 1]
-    ht += -t * (iu.c * ju.a + id.c * jd.a + ju.c * iu.a + jd.c * id.a)
-
-for i in range(L - 1):
-    hJ += J * (sops[i] * sops[i + 1])
+    ht += (-1 * t) * (iu.c * ju.a + id.c * jd.a + ju.c * iu.a + jd.c * id.a)  # t term
+    Si, Sj = sops[i], sops[i + 1]
+    hJ += (Si[0] * Sj[0]) + (Si[1] * Sj[1]) + (Si[2] * Sj[2])  # J term = Si.Sj
 
 h = ht + hJ
 qs.add_evolution(h, t)
